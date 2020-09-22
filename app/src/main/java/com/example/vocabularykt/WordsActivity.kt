39c9f10c436b452032fd574959229a27731293e4
Talancev <mishatalancev@gmail.com>
2020-09-22 13:01:25 +0300

    add_get_data_from_db

--- app/src/main/java/com/example/vocabularykt/WordsActivity.kt
+++ app/src/main/java/com/example/vocabularykt/WordsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.vocabularykt
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_words.*
 import java.util.*
 
@@ -10,15 +13,29 @@ class WordsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_words)
 
+        val db = Firebase.firestore
+
         val words: ArrayList<Word> = ArrayList<Word>()
 
-        words.add(
-            Word(
-                "dd",
-                "aa"
-            )
-        )
+        db.collection("words")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("get_word", "${document.id} => ${document.data}")
+                    words.add(Word(document.get("english") as String,
+                        document.get("russian") as String
+                    ))
+                }
 
-        words_list.adapter = WordAdapter(this@WordsActivity, words)
+                words.sort()
+                
+                words_list.adapter = WordAdapter(this@WordsActivity, words)
+
+            }
+            .addOnFailureListener { exception ->
+                Log.w("get_word", "Error getting documents.", exception)
+            }
+
+
     }
 }
